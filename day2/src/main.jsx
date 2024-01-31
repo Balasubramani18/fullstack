88c4d2a7e1f34ba9dd40a63f690410600b29293d
Balasubramani18 <113656274+Balasubramani18@users.noreply.github.com>
2024-01-31 16:28:13 +0530

    Add files via upload

---
+++ day2/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Ap from './Components/Card.jsx'
+import Home from './Pages/home.jsx'
+import Nav from './Components/Nav.jsx'
+import 'rsuite/dist/rsuite.min.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+)
